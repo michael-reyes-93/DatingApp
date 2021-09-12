23743aa7756812c7645f4f90c84cc02f2739c3ec
michael reyes <michaelreyesfernandez@hotmail.com>
2021-09-12 11:27:46 -0600

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,9 +22,6 @@ export class NavComponent implements OnInit {
     console.log(this.model)
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
